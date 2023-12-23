4bea6399689ac3e31511c544e978c7e05be81499
fslmc <fslfaisal17@gmail.com>
2023-12-24 06:56:15 +0700

    work
    database sudh connected ygu

--- portofolio/resources/views/home.blade.php
+++ portofolio/resources/views/home.blade.php
@@ -2,4 +2,28 @@
 <head></head>
 <body>
     <h3>ohayou skai</h3>
+    <h1>Data Siswa</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Nama</th>
+            <th>No. Telp</th>
+            <th>Alamat</th>
+            <th>Tanggal Lahir</th>
+            <th>Tempat Lahir</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($siswa as $data)
+            <tr>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->no_telp }}</td>
+                <td>{{ $data->alamat }}</td>
+                <td>{{ $data->tanggal_lahir }}</td>
+                <td>{{ $data->tempat_lahir }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
 </body>
